13ad22b5d11be6babdba98ab0d6dde83eb3e093f
Jherrerocavadas <jherrerocavadas@gmail.com>
2024-06-30 14:53:07 -0300

    feat: Organização das rotas da área logada

--- src/routes/SignedRoutes.tsx
+++ src/routes/SignedRoutes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { HorarioAula } from '../pages/HorarioAula';
+import { SeuHorario } from '../pages/SeuHorario';
 import { Matricula } from '../pages/Matricula';
 import { NotFound } from '../pages/NotFound';
 import { Home } from '../pages/Home';
@@ -14,14 +15,14 @@ function SignedRoutes(){
       <Route path="/" element={<HomeLogado />} />
       <Route path="/matricula" element={<Matricula />} />
       <Route path={"/horario-cursos/:siglaCurso"} element={<HorarioAula />} />
-      <Route path="/avisos" element={<Matricula />} />
-      <Route path="/seu-horario" element={<HorarioAula />} />
-      <Route path="/material-de-aula" element={<Matricula />} />
-      <Route path="/suas-notas" element={<Matricula />} />
-      <Route path="/suas-ausencias" element={<Matricula />} />
-      <Route path="/seu-historico" element={<Matricula />} />
-      <Route path="/calendario-academico" element={<Matricula />} />
-      <Route path="/emissao-de-documentos" element={<Matricula />} />
+      <Route path="/avisos" element={<NotFound />} />
+      <Route path="/seu-horario" element={<SeuHorario />} />
+      <Route path="/material-de-aula" element={<NotFound />} />
+      <Route path="/suas-notas" element={<NotFound />} />
+      <Route path="/suas-ausencias" element={<NotFound />} />
+      <Route path="/seu-historico" element={<NotFound />} />
+      <Route path="/calendario-academico" element={<NotFound />} />
+      <Route path="/emissao-de-documentos" element={<NotFound />} />
     </Routes>
   );
 };
