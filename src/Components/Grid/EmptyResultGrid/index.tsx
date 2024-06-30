c7adee107322ff6e217d80f72be99a46a64b7c85
Jherrerocavadas <jherrerocavadas@gmail.com>
2024-06-30 18:15:38 -0300

    feat: Criação de container de grid vazio

---
+++ src/Components/Grid/EmptyResultGrid/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./EmptyResultGridStyle.css"
+export default function EmptyResultGrid() {
+  return (
+    <div className='empty-grid-container'>Ops! Nenhum resultado foi encontrado.</div>
+  )
+}
